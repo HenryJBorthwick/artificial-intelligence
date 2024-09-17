e1160c5ee243f0353a649ee2ee7dc977157988a4
Henry Borthwick <hbo50@uclive.ac.nz>
2024-09-17 15:47:27 +1200

    peano numerials

---
+++ mid-sem-study/quiz4+/kb7.pl
@@ -0,0 +1,12 @@
+% prolog peano numerals
+
+% base case: zero is not positive
+natural(zero).
+
+% recursive case: a number is positive, if it is a successor of another natural number
+natural(succ(X)) :- natural(X).
+
+% Natural number, non-negative integer and not zero
+positive(succ(X)) :- natural(X).
+
+% strips off succ each loop
